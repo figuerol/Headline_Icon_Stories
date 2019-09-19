f0996862a786bbbe0bf8b834cc3f1916d81c5a59
Andres Figuerola <aefiguerola@gmail.com>
2019-09-18 20:46:27 -0400

    Creating a framework Flask app

--- app.py
+++ app.py
@@ -1,27 +1,16 @@
 from flask import Flask, render_template, request, redirect
-from bokeh.resources import CDN
-from bokeh.plotting import figure
-from bokeh.embed import components
-from bokeh.embed import json_item
-from bokeh.resources import INLINE
-# from bokeh.templates import RESOURCES
-from bokeh.util.string import encode_utf8
-from bokeh.util.browser import  view
-import pandas as pd
-
 import os
 import json
 import requests
-from bokeh.resources import INLINE
-from bokeh.palettes import Dark2_5 as palette
-
-
-
+from requests_oauthlib import OAuth1
+import webbrowser
 
 
 app = Flask(__name__)
 
-SECRET_KEY = os.getenv('SECRET_KEY', default = 'SECRET_KEY')
+NYTKEY = os.getenv('KEY_NYT', default = 'SECRET_KEY')
+NOUN_KEY=os.getenv('KEY_NOUN', default = 'SECRET_KEY')
+NOUN_SECRET=os.getenv('SECRET_NOUN', default = 'SECRET_KEY')
 app.vars={}
 
 @app.route('/')
@@ -34,23 +23,78 @@ def index():
     if request.method == "GET":
         return render_template('index.html')
     else:
-        app.vars['feature']= request.form.getlist('feature')
-        app.vars['ticker']=request.form['ticker']
-#getting data from API
-        url= 'https://www.quandl.com/api/v3/datasets/WIKI/{}/data.json?limit=5&api_key={}'.format(app.vars['ticker'], SECRET_KEY)
-        req = requests.get(url)
-        data = req.json()['dataset_data']['data']
-        columns= req.json()['dataset_data']['column_names']
+        app.vars['topic']=request.form['topic']
 
-        app.vars['data']=pd.DataFrame(data, columns=columns)
+        app.vars['snippet']=request.form['snippet']
+#getting data from NYT API  #WTforms package for WTforms
+#if you specify a topic it will return headline else just return the icon associated to the snippet
 
-        return redirect('/bok')
+        urlnyt= 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q={}&api-key={}'.format(app.vars['topic'], NYTKEY)
+        req = requests.get(urlnyt)
+        first_article = req.json()['response']['docs'][0]
+        lead_paragraph = first_article['lead_paragraph']
+        snippet = first_article['snippet']
+
+        try:
+            headline=first_article['headline']['print_headline']
+
+        except:
+            headline=first_article['headline']['main']
 
 
-@app.route('/bok')
-def bok():
 
-    return render_template('plot.html', ticker=app.vars['ticker'], resources=CDN.render())
+#getting data from Noun project
+        authnoun = OAuth1(NOUN_KEY, NOUN_SECRET)
+        try:#We check wether there was an assigned topic as default
+                app.vars['headline']=headline
+
+                endpoint = 'http://api.thenounproject.com/icon/{}'.format(app.vars['topic'])
+
+                response = requests.get(endpoint, auth=authnoun)
+                app.vars['icon_url']=response.json()["icon"]["preview_url"]
+
+        except: #if not we search for snippet
+                keywords= app.vars['snippet']
+
+
+                endpoint = 'http://api.thenounproject.com/icon/{}'.format(keywords)
+
+                response = requests.get(endpoint, auth=authnoun)
+
+                app.vars['icon_url']=response.json()["icon"]["preview_url"]
+                print(app.vars['icon_url'])
+        webbrowser.open(app.vars['icon_url'])
+
+
+
+        return redirect('/icon')
+
+
+
+@app.route('/icon')
+def icon():
+#if you specify a topic it will return headline and snippet, else just return the icon associated to the snippet
+    icon_topic='None'
+    icon_headline='None'
+    icon_snippet='None'
+    try:
+        icon_snippet=app.vars['snippet']
+
+    except:
+        None
+    try:
+        icon_topic=app.vars['topic']
+
+    except:
+        None
+    try:
+        icon_headline=app.vars['headline']
+
+    except:
+        None
+
+
+    return render_template('plot.html', icon_topic=icon_topic, icon_headline=icon_headline, icon_snippet=icon_snippet)#, resources=CDN.render())
 
 
 
@@ -59,18 +103,13 @@ def bok():
 def plot():
     plot=figure(x_axis_type="datetime")
 
-    # for  box in app.vars['feature']:
-    #     plot.line(pd.to_datetime(app.vars['data']['Date']),app.vars['data'][box], legend=box, color=palette[i], line_width=1)
-
     for i,box in enumerate(app.vars['feature']):
         print(box)
         plot.line(pd.to_datetime(app.vars['data']['Date']),app.vars['data'][box], color= palette[i], legend=box, line_width=1)
 
     return json.dumps(json_item(plot, "myplot"))
 
-@app.route('/about')
-def about():
-  return render_template('about.html')
+
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug = True)
